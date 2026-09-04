5cd8894c6ca3895d35ec9bc074c13f9a6996035a
Enzo Fox <firemonster612@icloud.com>
2026-09-04 03:36:17 +0100

    remove source-scanning capture test

--- tests/test_platform_composition.cpp
+++ tests/test_platform_composition.cpp
@@ -284,18 +284,6 @@ private slots:
         QCOMPARE(requested.count(), 1);
     }
 
-    void atSpiCaptureDoesNotSynthesizeAClipboardCopy()
-    {
-        const QString sourcePath = QDir(QStringLiteral(QT_TESTCASE_SOURCEDIR))
-                                       .filePath(QStringLiteral("src/platform/AtSpiTargetProvider.cpp"));
-        QFile source(sourcePath);
-        QVERIFY2(source.open(QIODevice::ReadOnly), qPrintable(sourcePath));
-        const QByteArray implementation = source.readAll();
-
-        QVERIFY(!implementation.contains("copySelection("));
-        QVERIFY(!implementation.contains("WlClipboardDelivery"));
-        QVERIFY(!implementation.contains("YdotoolDelivery"));
-    }
 #endif
 
 #ifdef Q_OS_LINUX
